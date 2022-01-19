2f74ec2bd657106c3add57e49a206602e787bc0a
girovizu <74430353+girovizu@users.noreply.github.com>
2022-01-19 11:41:23 -0500

    Update funciones.c

--- codigo/funciones.c
+++ codigo/funciones.c
@@ -35,3 +35,153 @@ ISR(INT0_vect){
 ISR(INT1_vect){
  secuencia = false;
 }
+
+ISR(ADC_vect){ //ENCERAR EL BIT 4 DEL REG ADCSRA &=~(1<<ADIF)
+   //Se muestra en el display el porcentaje de volúmen
+   displayV();
+}
+
+void SecLeds1(){  //Primera secuencia incremental normal
+   Vconv_leds = conversionLed();
+   tiraLed.clear(); //SE ELIMINA LOS VALORES PREVIOS DE LA TIRA LED
+   for(uint16_t i=0; i<Vconv_leds;i++){ //Se recorren para la cantidad de leds que se deben encender según la conversión ADC
+      if(i<N_PIXELS*0.1){ //Color para el 10% de los leds totales
+   tiraLed.setPixelColor(i,0,25,225); //AZUL
+      }
+      else if((N_PIXELS*0.1<=i)&(i<N_PIXELS*0.6)){ //Color para los leds totales entre 10 y 60 %
+   tiraLed.setPixelColor(i,0,255,25); //VERDE
+      }
+      else if((N_PIXELS*0.6<=i)&(i<N_PIXELS*0.85)){ //Color para los leds totales entre 60 y 85 %
+   tiraLed.setPixelColor(i,120,225,0); //AMARILLO
+      }
+      else{ //Color para el resto de leds totales
+   tiraLed.setPixelColor(i,255,0,0); //ROJO
+      }
+   }
+   delay(3); // si no se agrega un pequeño delay ocurren errores.
+   tiraLed.show();
+}
+
+void SecLeds2(){  //Segunda secuencia incremental desde las esquinas
+   Vconv_leds = conversionLed();
+   tiraLed.clear(); //SE ELIMINA LOS VALORES PREVIOS DE LA TIRA LED
+   //Se recorren los leds desde el inicio hasta la mitad del total de los leds que deben encenderse según el ADC y se refleja la misma secuencia en la otra mitad
+   for(uint16_t i=0; i<Vconv_leds/2;i++){
+      if(i<N_PIXELS*0.05){ //Color para los leds de entre (0-5)% y (95-100)% de la tira led
+   tiraLed.setPixelColor(i,0,25,255); //AZUL
+   tiraLed.setPixelColor(N_PIXELS-i-1,0,25,225);//AZUL
+      }
+      else if((N_PIXELS*0.05<=i)&(i<N_PIXELS*0.3)){ //Color para los leds entre (5-30)% y (70-95)% de la tira led
+   tiraLed.setPixelColor(i,0,255,25); //VERDE
+   tiraLed.setPixelColor(N_PIXELS-i-1,0,255,25);//VERDE
+      }
+      else if((N_PIXELS*0.3<=i)&(i<N_PIXELS*0.425)){ //Color para los leds entre (30-42.5)% y (57.5-70)% de la tira led
+   tiraLed.setPixelColor(i,120,225,0); //AMARILLO
+   tiraLed.setPixelColor(N_PIXELS-i-1,120,225,0);//AMARILLO
+      }
+      else{ //Color para el resto de los leds totales
+   tiraLed.setPixelColor(i,255,0,0); //ROJO
+   tiraLed.setPixelColor(N_PIXELS-i-1,255,0,0);//ROJO
+      }
+   }
+   delay(3); // si no se agrega un pequeño delay ocurren errores.
+   tiraLed.show();
+}
+
+void escogerSecuencia(){
+   if(secuencia){
+   SecLeds1();
+      }
+      else{
+   SecLeds2();
+      }
+}
+
+void mostrarNum(uint8_t numero){
+   switch(numero){
+      case 0:
+   PORTB = Num0;
+   PORTC = 0b1; //Segemento A
+   break;
+      case 1:
+   PORTB = Num1;
+   PORTC = 0b0; //Segemento A
+   break;
+      case 2:
+   PORTB = Num2;
+   PORTC = 0b1; //Segemento A
+   break;
+      case 3:
+   PORTB = Num3;
+   PORTC = 0b1; //Segemento A
+   break;
+      case 4:
+   PORTB = Num4;
+   PORTC = 0b0; //Segemento A
+   break;
+      case 5:
+   PORTB = Num5;
+   PORTC = 0b1; //Segemento A
+   break;
+      case 6:
+   PORTB = Num6;
+   PORTC = 0b1; //Segemento A
+   break;
+      case 7:
+   PORTB = Num7;
+   PORTC = 0b1; //Segemento A
+   break;
+      case 8:
+   PORTB = Num8;
+   PORTC = 0b1; //Segemento A
+   break;
+      case 9:
+   PORTB = Num9;
+   PORTC = 0b1; //Segemento A
+   break;
+   }
+}
+
+void displayV(){
+   //Separamos unidades
+   Vconv_display = conversionDisplay();
+   unidades = Vconv_display%10;
+   decenas = (Vconv_display/10)%10;
+   centenas = Vconv_display/100;
+   //Seleccionamos display
+   displaySelecter = displaySelecter<<1;
+   if (displaySelecter==32){
+      displaySelecter=1;
+   }
+   PORTD = ~displaySelecter;
+   //Mostramos el número
+   switch (displaySelecter){
+      case 1:
+   mostrarNum(unidades);
+   break;
+      case 2:
+   mostrarNum(decenas);
+   break;
+      case 4:
+   mostrarNum(centenas);
+   break;
+   }
+}
+
+uint16_t conversionDisplay(){ //Conversión ADC conveniente para el display.
+   while(ADCSRA & (1<< ADSC)); // ADSC DISABLED? 
+   Vconv_display= ADC/10.1488; // Convierte el rango de 0-1024 a 0-100 (Porcentaje de volumen)
+   return Vconv_display;
+}
+
+uint16_t conversionLed(){ //Conversión ADC conveniente para la tiraLed.
+   while(ADCSRA & (1<< ADSC)); // ADSC DISABLED? 
+   Vconv_leds= ADC*N_PIXELS/1023; // Convierte el rango de 0-1024 a 0-N_PIXELS (Leds que se encenderán)
+   return Vconv_leds; 
+}
+
+void brilloLeds(){ //Conversión ADC conveniente para la tiraLed.
+   while(ADCSRA & (1<< ADSC)); // ADSC DISABLED? 
+   Vconv_brillo= ADC/8.06; // Convierte en el rango para el nivel de brillo
+   tiraLed.setBrightness(Vconv_brillo); //Modifica el brillo de la tira led
+}
