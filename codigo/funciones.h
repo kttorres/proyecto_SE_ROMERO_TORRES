a47c129a011696b217362d05e7989664e1ae289b
kttorres <84658656+kttorres@users.noreply.github.com>
2022-01-19 03:38:13 -0500

    Create funciones.h

---
+++ codigo/funciones.h
@@ -0,0 +1,19 @@
+#include "config.h"
+
+void SecLeds1();
+
+void SecLeds2();
+
+void escogerSecuencia();
+
+void mostrarNum(uint8_t numero);
+
+void displayV();
+
+uint16_t conversionDisplay();
+
+uint16_t conversionLed();
+
+void brilloLeds();
+
+
