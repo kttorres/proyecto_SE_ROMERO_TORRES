c23e0e9a83aa096b742cca7322e88856c8643b8b
kttorres <84658656+kttorres@users.noreply.github.com>
2022-01-19 03:38:56 -0500

    Create config.h

---
+++ codigo/config.h
@@ -0,0 +1,15 @@
+#include <Adafruit_NeoPixel.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdbool.h>
+
+void interrupt_TIMER0_Init();
+
+void interrupt_INT0_Init();
+
+void interrupt_INT1_Init();
+
+void ADC_Init();
+
+void setUpp();
+
